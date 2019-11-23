0f451462aa30e6f8b88034d7a87679d5aeba9067
codecpacka <ramprasad.r2600@gmail.com>
2019-11-23 16:42:26 +0530

    in beta phase

---
+++ tree/binarytree.cpp
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define pf printf
+#define sf scanf
+
+struct bst
+{
+
+    struct bst *left;
+    int info;
+    struct bst *right;
+} * temp;
+
+struct bst *insbtree(struct bst *root, int data)
+{
+    struct bst *node;
+    node->left = NULL;
+    node->right = NULL;
+    node->info = data;
+
+    if (root == NULL)
+    {
+        root = node;
+        return root;
+    }
+    else
+    {
+        temp = root;
+        while (temp!=NULL)
+        {
+          
+            if (temp->info > data)
+            {
+                if (temp->left == NULL)
+                    temp->left = node;
+                temp = temp->left;
+            }
+            else if(temp->info < data)
+            {
+
+                if (temp->right == NULL)
+                    temp->right = node;
+                temp = temp->right;
+            }
+            else if (temp->info==data)
+                    temp=NULL;
+        }
+    }
+    return root;
+}
+
+void disbst(struct bst *root)
+{
+    if (root)
+    {
+        printf("%d \t", root->info);
+        disbst(root->left);
+        disbst(root->right);
+    }
+}
+
+int main()
+{
+    int data, ch;
+    struct bst *root;
+    root = NULL;
+    do
+    {
+        disbst(root);
+        pf("enter your choice\n ");
+        pf("1.insert\n");
+        pf("2.display\n");
+        pf("0.exit\n ");
+        sf("%d", &ch);
+        switch (ch)
+        {
+        case 0:
+            exit(0);
+            break;
+        case 1:
+            pf("x=");
+            sf("%d", &data);
+            root = insbtree(root, data);
+            break;
+
+        case 2:
+            disbst(root);
+            break;
+
+        default:
+            pf("wrong choice");
+            break;
+        };
+    } while (ch != 0);
+    return 0;
+}
