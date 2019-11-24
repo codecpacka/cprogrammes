0f93474c2c521dc02c696bcf55fd788355c95497
codecpacka <ramprasad.r2600@gmail.com>
2019-11-24 13:31:55 +0530

    search included

--- tree/binarytree.cpp
+++ tree/binarytree.cpp
@@ -2,10 +2,24 @@
 #include <stdlib.h>
 #define pf printf
 #define sf scanf
+#include "bst.h" ////////header file with all the functions used in this programme
 
-#include "bst.h"  ////////header file with all the functions used in this programme
-
-
+void search(struct bst *root, int data)
+{
+    if (root == NULL)
+    {
+        pf("\t element not found");
+        return;
+    }
+    else if (data < root->info)
+                    search(root->left, data);
+    else if (data > root->info)
+                    search(root->right, data);
+    else
+    {
+        pf(" \t element found ");
+    }
+}
 
 int main()
 {
@@ -18,6 +32,7 @@ int main()
         pf("\n enter your choice\n ");
         pf("1.insert\n");
         pf("2.display\n");
+        pf("3.search an element\n");
         pf("0.exit\n ");
         sf("%d", &ch);
         switch (ch)
@@ -33,6 +48,12 @@ int main()
 
         case 2:
             disbst(root);
+            break;
+
+        case 3:
+            pf("element search x=");
+            sf("%d", &data);
+            search(root, data);
             break;
 
         default:
